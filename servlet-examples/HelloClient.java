2dacc90878bcc559e3f7fd8b4f7279eae23e41fc
Seçkin Özden <seckin@Seckins-MacBook-Pro.local>
2017-02-08 21:01:56 +0300

    HelloClient service added

---
+++ servlet-examples/HelloClient.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class HelloClient extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String message;
+
+	public void init() throws ServletException {
+		// Do required initialization
+		message = "Hello Client";
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set response content type
+		response.setContentType("text/html");
+
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + "</h1>");
+	}
+
+	public void destroy() {
+		// do nothing.
+	}
+}

48b343c809954e0ece4cf0ded527fc54aa4dcee8
seckinozden <ozden.seckin@gmail.com>
2018-05-02 22:02:23 +0300

    jersey vs boot folder added

---
+++ jersey-vs-springboot/boot-exercises/src/main/java/com/seckin/bootexercises/BootExercisesApplication.java
@@ -0,0 +1,12 @@
+package com.seckin.bootexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootExercisesApplication.class, args);
+	}
+}
---
+++ jersey-vs-springboot/boot-exercises/src/test/java/com/seckin/bootexercises/BootExercisesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.seckin.bootexercises;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootExercisesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

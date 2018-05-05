d1b85792ac6fe6e2ca3a05a77eaad83bca2d5474
seckinozden <ozden.seckin@gmail.com>
2018-05-05 13:53:45 +0300

    response types updated to ResponseEntity

--- jersey-vs-springboot/boot-exercises/src/main/java/com/seckin/bootexercises/controller/ExerciseController.java
+++ jersey-vs-springboot/boot-exercises/src/main/java/com/seckin/bootexercises/controller/ExerciseController.java
@@ -4,8 +4,11 @@ import com.seckin.bootexercises.model.Exercise;
 import com.seckin.bootexercises.repository.ExerciseRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,45 +20,47 @@ public class ExerciseController {
     ExerciseRepository exerciseRepository;
 
     @RequestMapping(value = "exercises", method = RequestMethod.GET)
-    public List<Exercise> getAllExercises() {
-        return exerciseRepository.findAll();
+    public ResponseEntity<List<Exercise>> getAllExercises() {
+        return ResponseEntity.ok(exerciseRepository.findAll());
     }
 
     @RequestMapping(value = "exercises/{id}", method = RequestMethod.GET)
-    public Exercise getExercise(@PathVariable Long id) {
+    public ResponseEntity<?> getExercise(@PathVariable Long id) {
         Optional<Exercise> exercise = exerciseRepository.findById(id);
         if (exercise.isPresent())
-            return exercise.get();
+            return ResponseEntity.ok(exercise.get());
 
-        return null;
+        return ResponseEntity.notFound().build();
     }
 
-    @RequestMapping(value = "exercises/new/{id}", method = RequestMethod.POST)
-    public Exercise createExercise(@PathVariable Long id, @RequestBody Exercise exercise) {
-        exercise.setId(id);
-        return exerciseRepository.saveAndFlush(exercise);
+    @RequestMapping(value = "exercises/new", method = RequestMethod.POST)
+    public ResponseEntity<?> createExercise(@RequestBody Exercise exercise) {
+
+        Exercise result = exerciseRepository.saveAndFlush(exercise);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/" + result.getDescription()).buildAndExpand(result.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @RequestMapping(value = "exercises/update/{id}", method = RequestMethod.PUT)
-    public Exercise updateExercise(@PathVariable Long id, @RequestBody Exercise exercise) {
+    public ResponseEntity<Exercise> updateExercise(@PathVariable Long id, @RequestBody Exercise exercise) {
         Optional<Exercise> searchResult = exerciseRepository.findById(id);
         if (searchResult.isPresent()) {
             Exercise existingExercise = searchResult.get();
             BeanUtils.copyProperties(exercise, existingExercise);
-            return exerciseRepository.saveAndFlush(existingExercise);
+            return ResponseEntity.ok(exerciseRepository.saveAndFlush(existingExercise));
         }
-
-        return null;
+        return ResponseEntity.notFound().build();
     }
 
     @RequestMapping(value = "exercises/delete/{id}", method = RequestMethod.DELETE)
-    public String deleteExercise(@PathVariable Long id) {
+    public ResponseEntity<?> deleteExercise(@PathVariable Long id) {
 
         Optional<Exercise> searchResult = exerciseRepository.findById(id);
         if (searchResult.isPresent()) {
             exerciseRepository.deleteById(id);
-            return "Exercise deleted. ID=" + id;
+            return ResponseEntity.ok().build();
         }
-        return "Exercise with given ID not found! ID=" + id;
+        return ResponseEntity.notFound().build();
     }
 }

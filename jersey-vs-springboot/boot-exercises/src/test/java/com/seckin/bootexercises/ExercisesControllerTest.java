f824fa7bdeefa9637f77c143e75a3a2580118374
seckinozden <ozden.seckin@gmail.com>
2018-05-08 06:30:34 +0300

    implementing unit tests

---
+++ jersey-vs-springboot/boot-exercises/src/test/java/com/seckin/bootexercises/ExercisesControllerTest.java
@@ -0,0 +1,63 @@
+package com.seckin.bootexercises;
+
+import com.seckin.bootexercises.controller.ExerciseController;
+import com.seckin.bootexercises.model.Exercise;
+import com.seckin.bootexercises.repository.ExerciseRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class ExercisesControllerTest {
+
+    @InjectMocks
+    ExerciseController exerciseController;
+
+    @Mock
+    ExerciseRepository exerciseRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void test_getAllExercises(){
+
+        Exercise exercise = new Exercise((long)1,"Football","1 ball 22 men");
+        List<Exercise> exercises = new ArrayList(Arrays.asList(exercise));
+
+        Mockito.when(exerciseRepository.findAll()).thenReturn(exercises);
+
+        exerciseController.getAllExercises();
+
+        Mockito.verify(exerciseRepository).findAll();
+    }
+
+    @Test
+    public void test_getExercise(){
+        Exercise exercise = new Exercise((long)1,"Football","1 ball 22 men");
+        Optional<Exercise> optionalMock = Mockito.mock(Optional.class);
+
+        Mockito.when(optionalMock.get()).thenReturn(exercise);
+        Mockito.when(exerciseRepository.findById((long)1)).thenReturn(optionalMock);
+
+        exerciseController.getExercise((long)1);
+
+        Mockito.verify(exerciseRepository).findById((long)1);
+    }
+
+    //createExercise
+    //updateExercise
+    //deleteExercise
+
+}

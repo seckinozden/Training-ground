c4256356317ced793a90456d7874aa23bf0c3a01
seckinozden <ozden.seckin@gmail.com>
2018-05-10 05:56:18 +0300

    springboot folder renamed, food-tracking app added

---
+++ springboot/food-tracking/src/main/java/com/seckin/foodtracking/FoodTrackingApplication.java
@@ -0,0 +1,12 @@
+package com.seckin.foodtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTrackingApplication.class, args);
+	}
+}

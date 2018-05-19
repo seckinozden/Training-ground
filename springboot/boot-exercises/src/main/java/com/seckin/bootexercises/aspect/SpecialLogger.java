849b777b49de2e51129c9ad1409c85335bb2fdc2
seckinozden <ozden.seckin@gmail.com>
2018-05-19 13:46:47 +0300

    duplicated code refactored, before and after annotations added for the getByIdOperation aspect

--- springboot/boot-exercises/src/main/java/com/seckin/bootexercises/aspect/SpecialLogger.java
+++ springboot/boot-exercises/src/main/java/com/seckin/bootexercises/aspect/SpecialLogger.java
@@ -1,36 +1,32 @@
 package com.seckin.bootexercises.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class SpecialLogger {
 
-    @Around(value = "getByIdOperation()")
+    @Around(value = "getByIdOperation() || allControllerOperations()")
     public Object logGetByIdOperation(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         Object result = proceedingJoinPoint.proceed();
         long stop = System.currentTimeMillis();
         String methodName = proceedingJoinPoint.getSignature().getName();
-        System.err.println(String.format("%s runs %d ms", methodName, (stop - start)));
+        System.err.println(String.format("##  ATTENTION  ## %s runs %d ms", methodName, (stop - start)));
         return result;
     }
 
-
-    @Around(value = "allControllerOperations()")
-    public Object logAllControllerOperations(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
-        long start = System.currentTimeMillis();
-        Object result = proceedingJoinPoint.proceed();
-        long stop = System.currentTimeMillis();
-        String methodName = proceedingJoinPoint.getSignature().getName();
-        System.err.println(String.format("%s runs %d ms", methodName, (stop - start)));
-        return result;
+    @Before(value = "getByIdOperation()")
+    public void logBeforeGetByIdOperation() {
+        System.err.println("##  ATTENTION  ## I am working before the execution of the getByIdOperation() aspects");
     }
 
+    @After(value = "getByIdOperation()")
+    public void logAfterGetByIdOperation() {
+        System.err.println("##  ATTENTION  ## I am working after the execution of the getByIdOperation() aspects");
+    }
 
     @Pointcut("@annotation(com.seckin.bootexercises.aspect.LogProfile)")
     public void getByIdOperation() {
